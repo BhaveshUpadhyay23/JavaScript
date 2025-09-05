aeef7f5cdd6e92e0bfdc46e59b7eda8a2d325a78
Bhavesh Upadhyay <bhaveshu20@gmail.com>
2025-09-05 21:11:31 +0530

    update

--- Miniprojects/ToDo/todo.js
+++ Miniprojects/ToDo/todo.js
@@ -1,24 +1,46 @@
-let delBtn = document.querySelector(".delete");
-let doneBtn = document.querySelector(".done");
 let addBtn = document.querySelector(".add");
 let enteredTaskValu = document.querySelector(".enteredtask");
-
-delBtn.addEventListener("click",()=>{
-    console.log("Task Deleted Successfully");
-
-})
-
-doneBtn.addEventListener("click",()=>{
-    console.log("Task Completed");
-})
+let taskList = document.querySelector(".taskList");
+let listItem = document.querySelector(".listItem");
 
 const enteredTask = () => {
     console.log(enteredTaskValu.value);
-    let newTask = createElement("li");
-    newTask.textContent = enteredTaskValu.value;
+    let newTask = document.createElement("li");
+    newTask.classList.add("addedTasks");
+
+    let newTaskContent = document.createElement("p");
+    newTaskContent.textContent = enteredTaskValu.value;
+
+    let doneBtn = document.createElement("button");
+    doneBtn.innerHTML = "&#10003;";
+    doneBtn.classList.add("btn");
+    doneBtn.classList.add("done");
+
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "&cross;";
+    delBtn.classList.add("btn");
+    delBtn.classList.add("delete");
+
+    newTask.appendChild(doneBtn);
+    newTask.appendChild(newTaskContent)
+    newTask.appendChild(delBtn);
+
     console.log(newTask);
+
+    taskList.appendChild(newTask);  
+
+    console.log(taskList);
     enteredTaskValu.value = "";
 }
+
+// delBtn.addEventListener("click",()=>{
+//     console.log("Task Deleted Successfully");
+
+// })
+
+// doneBtn.addEventListener("click",()=>{
+//     console.log("Task Completed");
+// })
 
 addBtn.addEventListener("click",()=>{
     console.log("Task Added Successfully");
